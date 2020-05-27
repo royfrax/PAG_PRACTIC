e79eda2cc075ca5b0fb9a6277cb54c8fadba5b6b
royfrax <francisco17081992@gmail.com>
2020-05-26 20:59:49 -0500

    calculadora sencilla

---
+++ cod.js
@@ -0,0 +1,19 @@
+document.getElementById("n1").addEventListener("click",n1);
+document.getElementById("n2").addEventListener("click",n2);
+document.getElementById("n3").addEventListener("click",n3);
+document.getElementById("n4").addEventListener("click",n4);
+document.getElementById("n5").addEventListener("click",n5);
+document.getElementById("n6").addEventListener("click",n6);
+document.getElementById("n7").addEventListener("click",n7);
+document.getElementById("n8").addEventListener("click",n8);
+document.getElementById("n9").addEventListener("click",n9);
+document.getElementById("n0").addEventListener("click",n0);
+
+function n1() {
+	let actual = document.getElementById('resultado').innerHTML;
+	let sumado = document.getElementById("n1").innerHTML;
+    document.getElementById('resultado').innerHTML = actual + sumado
+}
+function numeroenpantalla1(){
+
+}
