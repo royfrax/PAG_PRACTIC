b23aa7d029ea3c452af2e7fb7f403df567e5aef6
royfrax <francisco17081992@gmail.com>
2020-06-15 00:18:20 -0500

    correccion de parrafos a tablas

--- cod.js
+++ cod.js
@@ -11,29 +11,31 @@ window.onload = function(){ //Acciones tras cargar la página
     var op="no"; //operación en curso; "no" =  sin operación.
     //mostrar número en pantalla según se va escribiendo:
     function numero(xx) { //recoge el número pulsado en el argumento.
-             if (x=="0" || xi==1  ) {	// inicializar un número, 
+             if (x=="0" || xi==1  ) // inicializar un número,
+                {	 
                 pantalla.innerHTML=xx; //mostrar en pantalla
                 x=xx; //guardar número
-                if (xx==".") { //si escribimos una coma al principio del número
+                if (xx=="."){ //si escribimos una coma al principio del número
                    pantalla.innerHTML="0."; //escribimos 0.
                    x=xx; //guardar número
                    coma=1; //cambiar estado de la coma
+                            }
                 }
-            }
-               else { //continuar escribiendo un número
-                   if (xx=="." && coma==0) { //si escribimos una coma decimal pòr primera vez
+                else { //continuar escribiendo un número
+                    if (xx=="." && coma==0) //si escribimos una coma decimal pòr primera vez
+                    { 
                     pantalla.innerHTML+=xx;
                     x+=xx;
-                       coma=1; //cambiar el estado de la coma  
+                    coma=1; //cambiar el estado de la coma  
                     }
                    //si intentamos escribir una segunda coma decimal no realiza ninguna acción.
                     else if (xx=="." && coma==1) {} 
                    //Resto de casos: escribir un número del 0 al 9 	 
-                else {
+                else{
                     pantalla.innerHTML+=xx;
                     x+=xx
-                }
-                }
+                    }
+                    }
                 xi=0 //el número está iniciado y podemos ampliarlo.
             }
     function operar(s) {
